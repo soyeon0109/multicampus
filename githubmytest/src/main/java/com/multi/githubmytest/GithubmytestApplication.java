524ea3be75d24ac6c26031c4a744611625cf4942
soyeon0109 <3937893@naver.com>
2021-04-27 10:40:56 +0900

    mytest commit

---
+++ githubmytest/src/main/java/com/multi/githubmytest/GithubmytestApplication.java
@@ -0,0 +1,13 @@
+package com.multi.githubmytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubmytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubmytestApplication.class, args);
+	}
+
+}
---
+++ githubmytest/src/test/java/com/multi/githubmytest/GithubmytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.githubmytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubmytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
